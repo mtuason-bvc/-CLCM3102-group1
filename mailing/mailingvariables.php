8bf2367d3c7b12999454b62d1cd0b801971414f8
Naco00642 <E.EGOBUEZE201@MYBVC.CA>
2024-10-31 12:00:56 -0600

    Add files via upload

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "ford42avas@gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = ""; //sender
+    $mail_sender_password = ""; //sender
+    $mail_sender_name = "Website Form";
+?>
